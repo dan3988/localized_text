7db5091791c49026f13375ac5ba0ba3f7d01e891
dan3988 <40858622+dan3988@users.noreply.github.com>
2025-03-25 15:18:56 +0000

    Added documentation

--- core/lib/src/getter.dart
+++ core/lib/src/getter.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/widgets.dart';
 
+/// Getter for the translation of a [String] for the locale in a given [BuildContext].
 abstract class LocalizedText {
+  /// If the given [text] is null, it falls back to the provided [fallback] instance.
   static LocalizedText fallback(String? text, LocalizedText fallback) =>
       text == null ? fallback : _StaticLocalizedText(text);
 
@@ -8,6 +10,7 @@ abstract class LocalizedText {
 
   const factory LocalizedText.static(String text) = _StaticLocalizedText;
 
+  /// Retrieve the localized string for the given context
   String get(BuildContext context);
 }
 
@@ -20,6 +23,7 @@ final class _StaticLocalizedText extends LocalizedText {
   get(_) => text;
 }
 
+/// Uses the instance of [T] in the given [BuildContext] to get a translated [String].
 abstract class LocalizedTextGetter<T extends Object> extends LocalizedText {
   const LocalizedTextGetter() : super();
   const factory LocalizedTextGetter.getter(
@@ -27,7 +31,7 @@ abstract class LocalizedTextGetter<T extends Object> extends LocalizedText {
 
   @override
   get(context) {
-    final localizations = Localizations.of(context, T);
+    final localizations = Localizations.of<T>(context, T);
     assert(localizations != null, 'No localizations in scope.');
     return getFor(localizations!);
   }
--- core/lib/src/widget.dart
+++ core/lib/src/widget.dart
@@ -2,21 +2,49 @@ import 'package:flutter/widgets.dart';
 
 import 'getter.dart';
 
+/// Creates a [Text] widget showing the translation of [text] for the current context's language.
 @immutable
 class Localized extends StatelessWidget {
+  /// The getter for the text to display
   final LocalizedText text;
+
+  /// The value to be passed to [Text.style]
   final TextStyle? style;
+
+  /// The value to be passed to [Text.strutStyle]
   final StrutStyle? strutStyle;
+
+  /// The value to be passed to [Text.textAlign]
   final TextAlign? textAlign;
+
+  /// The value to be passed to [Text.textDirection]
   final TextDirection? textDirection;
+
+  /// The value to be passed to [Text.locale]
   final Locale? locale;
+
+  /// The value to be passed to [Text.softWrap]
   final bool? softWrap;
+
+  /// The value to be passed to [Text.overflow]
   final TextOverflow? overflow;
+
+  /// The value to be passed to [Text.textScaler]
   final TextScaler? textScaler;
+
+  /// The value to be passed to [Text.maxLines]
   final int? maxLines;
+
+  /// The value to be passed to [Text.semanticsLabel]
   final String? semanticsLabel;
+
+  /// The value to be passed to [Text.textWidthBasis]
   final TextWidthBasis? textWidthBasis;
+
+  /// The value to be passed to [Text.textHeightBehavior]
   final TextHeightBehavior? textHeightBehavior;
+
+  /// The value to be passed to [Text.selectionColor]
   final Color? selectionColor;
 
   const Localized({
