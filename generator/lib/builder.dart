f8cdd3d1a023b64ba51582063a76f399dc934e91
dan3988 <40858622+dan3988@users.noreply.github.com>
2025-03-25 12:04:05 +0000

    Show error if synthetic-package is not set to false in l10n.yaml

--- generator/lib/builder.dart
+++ generator/lib/builder.dart
@@ -9,30 +9,37 @@ import 'package:yaml/yaml.dart';
 import 'src/builder.dart';
 import 'src/settings.dart';
 
-Builder localizedWidgetGenerator(BuilderOptions options) {
-  final Generator generator;
+Generator _createGenerator(BuilderOptions options) {
   final file = File('l10n.yaml');
   if (!file.existsSync()) {
-    log.warning('l10n.yaml does not exist.');
-    generator = const _EmptyGenerator();
-  } else {
-    final text = file.readAsStringSync();
-    final value = loadYaml(text);
-    final String directory = value['output-dir'] ?? value['arb-dir'];
-    //final String templateName = value['template-arb-file'] ?? 'app_en.arb';
-    final String outputFile = value['output-localization-file'] ?? 'app_localizations.dart';
-    final bool? noNamedParameters = value['no-use-named-parameters'];
-    final bool namedParameters = noNamedParameters != null ? !noNamedParameters : value['use-named-parameters'] ?? false;
-    final className = value['output-class'] ?? 'AppLocalizations';
-    generator = LocalizedTextKeyGenerator(
-      //templatePath: '$directory/$templateName',
-      classPath: '$directory/$outputFile',
-      className: className,
-      namedParameters: namedParameters,
-      settings: Settings.fromJson(options.config),
-    );
+    log.warning('localized_text_key_generator: l10n.yaml does not exist.');
+    return const _EmptyGenerator();
   }
 
+  final text = file.readAsStringSync();
+  final YamlMap value = loadYaml(text);
+  if (value['synthetic-package'] != false) {
+    log.warning('localized_text_key_generator: l10n.yaml must have "synthetic-package: false".');
+    return const _EmptyGenerator();
+  }
+
+  final String directory = value['output-dir'] ?? value['arb-dir'];
+  //final String templateName = value['template-arb-file'] ?? 'app_en.arb';
+  final String outputFile = value['output-localization-file'] ?? 'app_localizations.dart';
+  final bool? noNamedParameters = value['no-use-named-parameters'];
+  final bool namedParameters = noNamedParameters != null ? !noNamedParameters : value['use-named-parameters'] ?? false;
+  final className = value['output-class'] ?? 'AppLocalizations';
+  return LocalizedTextKeyGenerator(
+    //templatePath: '$directory/$templateName',
+    classPath: '$directory/$outputFile',
+    className: className,
+    namedParameters: namedParameters,
+    settings: Settings.fromJson(options.config),
+  );
+}
+
+Builder localizedWidgetGenerator(BuilderOptions options) {
+  final generator = _createGenerator(options);
   return LibraryBuilder(generator, generatedExtension: '.keys.dart');
 }
 
